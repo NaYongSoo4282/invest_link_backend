15656081c85efd4a108b023a093c0c43db7323c9
kimseungjun <qps8622@gmail.com>
2026-03-11 11:54:44 +0900

    chore: Spring Boot 뼈대 생성

---
+++ src/main/java/creative/invest_link_backend/InvestLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package creative.invest_link_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestLinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestLinkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/creative/invest_link_backend/InvestLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package creative.invest_link_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestLinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
